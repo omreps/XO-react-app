bc9bf1ef1fa00923a2b2328beed898ee3e15c063
omreps <40422764+omreps@users.noreply.github.com>
2019-02-11 10:50:08 +0300

    Header added

---
+++ xo-app/src/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="Header-body">
+          <img src={logo} className="Header-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="Header-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
